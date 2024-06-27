84ab43a2d49d7bc13a2f1758387d5a92c47d4b7c
Melyenn <thiduyen310@gmail.com>
2024-06-27 10:05:59 +0700

    sua flash-sort

--- src/sortSource/flash-sort.cpp
+++ src/sortSource/flash-sort.cpp
@@ -137,66 +137,70 @@ void FlashSort::flashSortComparisonCount(int n, int *a)
     // tính số lớp cần dùng với công thức m = int(0, 45 * n)
     int min_val = a[0], max_val = a[0], m = int(0.45 * n);
 
-    // là mảng chứa xem mỗi lớp có vi tri cuoi la ở đâu
-    int *l = new int[m];
-    for (int i = 0; ++this->comparison && i < m; i++)
-        l[i] = 0;
+    if (++this->comparison && m > 1) {
+        // là mảng chứa xem mỗi lớp có vi tri cuoi la ở đâu
+        int *l = new int[m];
+        for (int i = 0; ++this->comparison && i < m; i++)
+            l[i] = 0;
 
-    // tim min, max
-    for (int i = 1; ++this->comparison && i < n; i++)
-    {
-        if (++this->comparison && a[i] < min_val)
+        // tim min, max
+        for (int i = 1; ++this->comparison && i < n; i++)
         {
-            min_val = a[i];
+            if (++this->comparison && a[i] < min_val)
+            {
+                min_val = a[i];
+            }
+            if (++this->comparison && a[i] > max_val)
+            {
+                max_val = a[i];
+            }
         }
-        if (++this->comparison && a[i] > max_val)
+
+        // min = max -> chi co 1 gia tri trong mang => return
+        if (++this->comparison && max_val == min_val)
+            return;
+
+        // tìm xem mỗi lớp có bao nhiêu số
+        int idx_m = m - 1;
+        for (int i = 0; ++this->comparison && i < n; i++)
         {
-            max_val = a[i];
+            int idx = get_class_id(a[i], min_val, max_val, m);
+            this->comparison++;
+            l[idx]++;
         }
-    }
 
-    // min = max -> chi co 1 gia tri trong mang => return
-    if (++this->comparison && max_val == min_val)
-        return;
-
-    // tìm xem mỗi lớp có bao nhiêu số
-    int idx_m = m - 1;
-    for (int i = 0; ++this->comparison && i < n; i++)
-    {
-        int idx = get_class_id(a[i], min_val, max_val, m);
-        this->comparison++;
-        l[idx]++;
-    }
-
-    // gán lại để biết vitri cua của lớp đó ở đâu trong mảng
-    for (int i = 1; ++this->comparison && i < m; i++)
-    {
-        l[i] += l[i - 1];
-    }
-
-    int cnt = 0, i = 0, id = m - 1;
-    while (++this->comparison && cnt < n - 1)
-    {
-        // nếu class id đã được sắp xếp xog -> tăng i để tìm class mới để xét
-        while (++this->comparison && i >= l[id])
+        // gán lại để biết vitri cua của lớp đó ở đâu trong mảng
+        for (int i = 1; ++this->comparison && i < m; i++)
         {
-            i++;
-            id = get_class_id(a[i], min_val, max_val, m);
-            ++this->comparison;
+            l[i] += l[i - 1];
         }
-        int tmp = a[i];
-        // dịch chuyển các số lần lượt về đúng lớp theo thứ tự từ trái qua phải của từng lớp
-        while (++this->comparison && i < l[id])
-        {
-            id = get_class_id(tmp, min_val, max_val, m);
-            ++this->comparison;
-                  l[id]--;
-            swap(&tmp, &a[l[id]]);
-            cnt++;
-        }
-    }
 
-    insertionSortComparisonCount(a, n);
+        int cnt = 0, i = 0, id = m - 1;
+        while (++this->comparison && cnt < n - 1)
+        {
+            // nếu class id đã được sắp xếp xog -> tăng i để tìm class mới để xét
+            while (++this->comparison && i >= l[id])
+            {
+                i++;
+                id = get_class_id(a[i], min_val, max_val, m);
+                ++this->comparison;
+            }
+            int tmp = a[i];
+            // dịch chuyển các số lần lượt về đúng lớp theo thứ tự từ trái qua phải của từng lớp
+            while (++this->comparison && i < l[id])
+            {
+                id = get_class_id(tmp, min_val, max_val, m);
+                ++this->comparison;
+                    l[id]--;
+                swap(&tmp, &a[l[id]]);
+                cnt++;
+            }
+        }
+        insertionSortComparisonCount(a, n);
+    }
+    else {
+        insertionSortComparisonCount(a, n);
+    }
 }
 
 
