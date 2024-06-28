d9b078c6a2ebaab4c95f68e4e3a53188873ab95f
TKin <letrungkienthd@gmail.com>
2024-06-28 10:08:03 +0700

    modified bin insertion sort

--- src/sortSource/binary-insertion-sort.cpp
+++ src/sortSource/binary-insertion-sort.cpp
@@ -15,11 +15,13 @@ BinaryInsertionSort::BinaryInsertionSort(int *arr, int n)
         this->tempArr2[i] = arr[i];
     }
 }
+
 BinaryInsertionSort::~BinaryInsertionSort()
 {
     delete[] tempArr;
     delete[] tempArr2;
 }
+
 int BinaryInsertionSort::BinarySearchRunningTimeCount(int* a, int item, int left, int right)
 {
     while (right > left) {
@@ -31,26 +33,23 @@ int BinaryInsertionSort::BinarySearchRunningTimeCount(int* a, int item, int left
     
     return (item > a[left]) ? (left + 1) : left;
 }
+
 void BinaryInsertionSort::BinaryInsertionSortRunningTimeCount(int *arr, int n)
 {
-    int i, index, j, k, selected;
- 
+    int i, j, index, key;
+
     for (i = 1; i < n; i++)
     {
-        j = i - 1;
-        selected = arr[i];
- 
-        index = BinarySearchRunningTimeCount(arr, selected, 0, j);
- 
-        while (j >= index)
-        {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        
-        arr[j + 1] = selected;
+        key = arr[i];
+        index = BinarySearchRunningTimeCount(arr, arr[i], 0, i - 1);
+
+        for (j = i; j > index; j--)
+            arr[j] = arr[j - 1];
+
+        arr[index] = key;
     }
 }
+
 int BinaryInsertionSort::BinarySearchWithComparisons(int* a, int item, int left, int right)
 {
     while (++this->comparison && right > left) {
@@ -62,31 +61,29 @@ int BinaryInsertionSort::BinarySearchWithComparisons(int* a, int item, int left,
     
     return (++this->comparison && item > a[left]) ? (left + 1) : left;
 }
+
 void BinaryInsertionSort::BinaryInsertionSortComparisonCount(int *arr, int n)
 {
-    int i, index, j, k, selected;
- 
-    for (i = 1; ++this->comparison && i < n; i++)
-    {
-        j = i - 1;
-        selected = arr[i];
- 
-        index = BinarySearchWithComparisons(arr, selected, 0, j);
- 
-        while (++this->comparison && j >= index)
-        {
-            arr[j + 1] = arr[j];
-            j--;
-        }
+    int i, j, index, key;
 
-        arr[j + 1] = selected;
+    for (i = 1; ++this->comparison &&  i < n; i++)
+    {
+        key = arr[i];
+        index = BinarySearchWithComparisons(arr, arr[i], 0, i - 1);
+
+        for (j = i; ++this->comparison && j > index; j--)
+            arr[j] = arr[j - 1];
+
+        arr[index] = key;
     }
 }
+
 int64_t BinaryInsertionSort::getComparison()
 {
     BinaryInsertionSortComparisonCount(this->tempArr2, this->size);
     return this->comparison;
 }
+
 double BinaryInsertionSort::getRunningTime()
 {
     // time in mili seconds
@@ -97,14 +94,17 @@ double BinaryInsertionSort::getRunningTime()
     this->runningTime = duration.count();
     return this->runningTime;
 }
+
 int* BinaryInsertionSort::getTempArr()
 {
     return this->tempArr;
 }
+
 int* BinaryInsertionSort::getTempArr2()
 {
     return this->tempArr2;
 }
+
 int BinaryInsertionSort::getSize()
 {
     return this->size;
